dcf21348ece47c43aed5bd342948f664e42eeac8
YouKlike <w93816@gmail.com>
2022-09-13 00:31:48 +0800

    change d060.cpp code

--- C++/d060.cpp
+++ C++/d060.cpp
@@ -3,8 +3,5 @@ using namespace std;
 int main(){
      int a; 
      cin >> a;
-     if (a >= 25)
-          cout << (60-a+25)%60 << endl;
-     else if (a < 25)
-          cout << abs(25-a) << endl;
+     cout << (60-a+25)%60 << endl;
 }
